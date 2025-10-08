ccddaee9389b4e8e486f831acce25362a9767e9f
Andreas Rudén <andreasruden91@gmail.com>
2025-10-08 17:57:15 +0200

    {common} Add panic macro

---
+++ src/common/debug/panic.h
@@ -0,0 +1,22 @@
+#ifndef DEBUG_PANIC__H
+#define DEBUG_PANIC__H
+
+#include <stdio.h>
+
+#define panic(msg, ...) do { \
+    fprintf(stderr, "PANIC at %s:%d in %s(): " msg "\n", \
+            __FILE__, __LINE__, __func__, ##__VA_ARGS__); \
+    fflush(stderr); \
+    __builtin_trap(); \
+} while(0)
+
+#define panic_if(cond) do { \
+    if (cond) { \
+        fprintf(stderr, "PANIC at %s:%d in %s(): assertion failed: %s\n", \
+                __FILE__, __LINE__, __func__, #cond); \
+        fflush(stderr); \
+        __builtin_trap(); \
+    } \
+} while(0)
+
+#endif
